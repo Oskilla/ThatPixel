469d4f3e0294f6fa26894e06e4982bccae205eea
Oskilla <questionaoskilla@hotmail.fr>
2020-06-17 15:19:37 +0200

    Create a panel and a jar executable

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-"# ThatPixel" 
+"# ThatPixel 1.1" 
+
 I was bored so I made this small program that take an image and two coordinate and return the RGB value of the given pixel
 
-17/06/2020 - Initial release
+17/06/2020 1.0- Initial release
+
+17/06/2020 1.1- Create a panel and a jar executable
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,69 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class Main  {
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Path to image");
-		String path = sc.nextLine();
-		BufferedImage img =  ImageIO.read(new FileInputStream(path));
-		System.out.println("Enter X coord");
-		int	coordX = sc.nextInt();
-		System.out.println("Enter Y coord");
-		int coordY = sc.nextInt();
-		Color col = new Color(img.getRGB(coordX,coordY));
-		System.out.println(col.getRed()+" "+col.getGreen()+" "+col.getBlue());
+		//Scanner sc = new Scanner(System.in);
+		JFrame fen = new JFrame();
+		fen.setTitle("That Pixel");
+		fen.setSize(800, 150);
+		fen.setLocationRelativeTo(null);
+		fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel pan = new JPanel();
+		JLabel lab1 = new JLabel("Path :");
+		pan.add(lab1);
+		JTextField jt1 = new JTextField(70);
+		pan.add(jt1);
+		JLabel lab2 = new JLabel("Coord X :");
+		pan.add(lab2);
+		JTextField jt2 = new JTextField(10);
+		pan.add(jt2);
+		JLabel lab3 = new JLabel("Coord Y :");
+		pan.add(lab3);
+		JTextField jt3 = new JTextField(10);
+		pan.add(jt3);
+		JButton bouton = new JButton("Get that pixel");
+		bouton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String path = jt1.getText();
+				int coordX = Integer.parseInt(jt2.getText());
+				int coordY = Integer.parseInt(jt3.getText());
+				BufferedImage img;
+				try {
+					img = ImageIO.read(new FileInputStream(path));
+					Color col = new Color(img.getRGB(coordX,coordY));
+					String o = col.getRed()+" "+col.getGreen()+" "+col.getBlue();
+					JLabel lab4 = new JLabel(o);
+					pan.add(lab4);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+			
+		});
+		pan.add(bouton);
+		fen.setContentPane(pan);
+		fen.setVisible(true);
+
 	}
 }
